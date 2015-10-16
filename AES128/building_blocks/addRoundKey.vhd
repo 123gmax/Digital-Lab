45451c5deeff1191b3db99fcdb40b1deca4dc0c3
BlackRage83 <BlackRage83@users.noreply.github.com>
2015-10-16 09:24:37 +0800

    Made addRoundKey into a synchronic component.

--- AES128/building_blocks/addRoundKey.vhd
+++ AES128/building_blocks/addRoundKey.vhd
@@ -22,17 +22,10 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity addRoundKey is
-    Port ( wordIn : in STD_LOGIC_VECTOR (31 downto 0);
+    Port ( CLK : in STD_LOGIC;
+           RESET : in STD_LOGIC;
+           wordIn : in STD_LOGIC_VECTOR (31 downto 0);
            keyIn : in STD_LOGIC_VECTOR (31 downto 0);
            wordOut : out STD_LOGIC_VECTOR (31 downto 0));
 end addRoundKey;
@@ -40,5 +33,12 @@ end addRoundKey;
 architecture Behavioral of addRoundKey is
 
 begin
-    wordOut <= wordIn XOR keyIn;
+    process(CLK, RESET, wordIn, keyIn)
+    begin
+        if RESET = '1' then
+            wordOut <= (others => '0');
+        elsif rising_edge(CLK) then
+            wordOut <= wordIn XOR keyIn;
+        end if;
+    end process;
 end Behavioral;
